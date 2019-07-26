4fcbaa1bc29f2b1cf0b744744e36d33977a00dd5
jacobt412 <53330839+jacobt412@users.noreply.github.com>
2019-07-26 00:22:40 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo "Hello World"
+
+curl -s https://raw.githubusercontent.com/typekpb/oradown/master/oradown.sh  | bash -s -- --cookie=accept-weblogicserver-server --username=$1 --password=$2 http://download.oracle.com/otn/nt/middleware/12c/12213/fmw_12.2.1.3.0_wls_Disk1_1of1.zip
