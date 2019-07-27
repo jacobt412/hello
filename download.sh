4f2ca2ab701132b6591adb99b2e536e4cef07892
jacobt412 <53330839+jacobt412@users.noreply.github.com>
2019-07-26 17:27:45 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,18 @@
-#!/bin/bash
-echo "Hello World"
+!/bin/bash
+echo "Inside customscript.sh"
+
+echo "Adding a new user"
+useradd oracle
+
+su - oracle
+echo "Get new user home directory"
+
+eval echo "~$different_user" 
+
+pwd
+
+ls -ltr
 
 curl -s https://raw.githubusercontent.com/typekpb/oradown/master/oradown.sh  | bash -s -- --cookie=accept-weblogicserver-server --username=$1 --password=$2 http://download.oracle.com/otn/nt/middleware/12c/12213/fmw_12.2.1.3.0_wls_Disk1_1of1.zip
+
+ls -ltr
