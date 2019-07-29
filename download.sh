1d447bf17aa9ebdbaf1766f76ddb8a3d3c3fb2a6
jacobt412 <53330839+jacobt412@users.noreply.github.com>
2019-07-28 20:33:30 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,14 +1,22 @@
 !/bin/bash
 echo "Inside customscript.sh"
 
+get_ora_home() {
+  local user_home; 
+  user_home="$(getent passwd "$1")" || return
+  echo $result | cut -d : -f 6
+}
+
 echo "Adding a new user"
-useradd oracle
+useradd -m oracle
 
 #su - oracle
 echo "Get new user home directory"
-eval echo "~oracle" 
+#eval echo "~oracle" 
 
-ora_user_home = $(getent passwd oracle|cut -d\: -f 6);
+ora_user_home = "$(get_ora_home oracle)"
+
+echo "oracle home is '$ora_user_home'"
 
 mkdir -p $ora_user_home/installables
 cd $ora_user_home/installables
@@ -22,3 +30,4 @@ ls -ltr
 curl -s https://raw.githubusercontent.com/typekpb/oradown/master/oradown.sh  | bash -s -- --cookie=accept-weblogicserver-server --username=$1 --password=$2 http://download.oracle.com/otn/nt/middleware/12c/12213/fmw_12.2.1.3.0_wls_Disk1_1of1.zip
 
 ls -ltr
+
